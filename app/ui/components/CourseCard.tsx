4659a906af794c06d91c140c88e46a481490e6fc
1TheCrazy <3thecrazy@gmail.com>
2025-07-26 15:48:05 +0200

    Refactor CourseCard to use style prop

--- app/ui/components/CourseCard.tsx
+++ app/ui/components/CourseCard.tsx
@@ -1,25 +1,20 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { View, Text, StyleProp, ViewStyle } from 'react-native';
 
 import { useTheme } from '../ThemeProvider';
 
-interface CourseWidgetProps{
-  name: string
-}
-
-export const CourseCard: React.FC<CourseWidgetProps> = ({name}) => {
+export const CourseCard: React.FC<{name: string, style?: StyleProp<ViewStyle>}> = ({name, style}) => {
   const { colors } = useTheme();
   
   return (
-    <View style={{
+    <View style={[{
         alignSelf: 'flex-start',
         backgroundColor: 'rgba(0, 0, 0, 0.4)',
         borderRadius: 10,
         padding: 5,
         borderColor: colors.courseCardBorderColor,
         borderWidth: 2
-      }}>
+      }, style]}>
       <Text style={{
         color: colors.fontColor
       }}>{name}</Text>
