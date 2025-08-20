69edd12ff7fa90b973dbafbf427d16f6e58402ea
1TheCrazy <3thecrazy@gmail.com>
2025-08-20 18:02:21 +0200

    Add onPress to coursecard

--- app/ui/components/CourseCard.tsx
+++ app/ui/components/CourseCard.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import { View, Text, StyleProp, ViewStyle } from 'react-native';
+import { View, Text, StyleProp, ViewStyle, Pressable } from 'react-native';
 
 import { useTheme } from '../ThemeProvider';
 
-export const CourseCard: React.FC<{name: string, style?: StyleProp<ViewStyle>}> = ({name, style}) => {
+export const CourseCard: React.FC<{name: string, onPress: () => void, style?: StyleProp<ViewStyle>}> = ({name, style, onPress}) => {
   const { colors } = useTheme();
   
   return (
-    <View style={[{
+    <Pressable 
+      onPress={() => onPress()}
+      style={[{
         alignSelf: 'flex-start',
         backgroundColor: 'rgba(0, 0, 0, 0.4)',
         borderRadius: 10,
@@ -18,6 +20,6 @@ export const CourseCard: React.FC<{name: string, style?: StyleProp<ViewStyle>}> 
       <Text style={{
         color: colors.fontColor
       }}>{name}</Text>
-    </View>
+    </Pressable>
   );
 }
