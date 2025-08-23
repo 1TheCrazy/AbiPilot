4e8da2ea4ed15efd822ac4a52ec820b1252aff87
1TheCrazy <3thecrazy@gmail.com>
2025-08-23 16:54:22 +0200

    Refactoring of interface

--- app/client/static/interfaces/CountryState.ts
+++ app/client/static/interfaces/CountryState.ts
@@ -1,14 +1,14 @@
 import ManagedCourse from "../implemented/ManagedCourse";
 import Course from "./Course";
-import PointWeights from "./PointWeights";
+import Exam from "./Exam";
 import UserCourse from "./UserCourse";
 
 export default interface CountryState{
     mandatoryCourses: ReadonlyArray<Course>, 
     mandatoryNumberOfCourses: number, // The number of courses you need to let a grade count for the final grade
-    pointWeights: PointWeights,
+    totalPoints(exams: Exam[]): number, // Total number of points given the queried Exams (should be virtual and non-virtual)
     nextCourses(currenCourses: ManagedCourse[]): ManagedCourse[], // A method for creating a valid arrangement of courses in each state depending on the courses chosen until now
-    PPE(grade: number): number[], //Returns the number of Point Per Exam the user needs to score to achieve his/her grade goal 
-                                  //First Entry are the points in a LK, Second Entry are the points in a BK, Third Entry are the points in a written Exam, Fourth Entry are the points in an oral Exam
-    lowPointsTip: string[], // An array of tips a user can follow if he/she didn't reach his/her grade goal
+    PPE(grade: number): number[], // Returns the number of Point Per Exam the user needs to score to achieve his/her grade goal 
+                                  // First Entry are the points in a LK, Second Entry are the points in a BK, Third Entry are the points in a written Exam, Fourth Entry are the points in an oral Exam
+    lowPointsTip: string[], // An array of tips a user can follow if he/she has a generally low grade
 }
